4dc1de4486585ce3a897a5a4b931e5253216b866
cjb412 <cdfcblank@gmail.com>
2024-10-28 17:20:02 -0400

    Simplified root page with new example test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders front page text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const paragraphElement = screen.getByText(/Front page example/i);
+  expect(paragraphElement).toBeInTheDocument();
 });
